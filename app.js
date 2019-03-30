22711e8d4998b120bb90d1b57d8ca6263c436f57
Alex C. Rocha <rocha.amc@gmail.com>
2019-03-29 23:11:02 -0700

    add listen to port 3000 if no environment variable PORT exists

--- app.js
+++ app.js
@@ -50,6 +50,6 @@ app.use("/", indexRoutes);
 app.use("/jobs", jobRoutes);
 app.use("/jobs/:id/comments", commentRoutes);
 
-app.listen(process.env.PORT, process.env.IP, function(){
+app.listen(process.env.PORT || 3000, process.env.IP, function(){
 	console.log("TechniApp Server is Online");
 });
