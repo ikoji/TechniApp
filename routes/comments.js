9ae63b3703af8d7b4185a53603a5604d5300c250
Alex C Rocha <ikoji@users.noreply.github.com>
2019-07-08 15:53:14 -0700

    Increase Comment char length

--- routes/comments.js
+++ routes/comments.js
@@ -1,68 +1,79 @@
 var express = require("express"),
-    router  = express.Router({mergeParams: true}),
-    Job     = require("../models/job"),
-    Comment = require("../models/comment"),
-    middleware = require("../middleware");
-const { check, validationResult } = require('express-validator/check');
+	router = express.Router({
+		mergeParams: true
+	}),
+	Job = require("../models/job"),
+	Comment = require("../models/comment"),
+	middleware = require("../middleware");
+const {
+	check,
+	validationResult
+} = require('express-validator/check');
 
 router.get("/new", middleware.isLoggedIn, newComment);
 router.post("/", middleware.isLoggedIn,
-			[
-			check("comment[text]")
-				.trim().escape()
-				.not().isEmpty()
-				.withMessage("Comment is empty")
-				.isLength({max:400}).withMessage("Too long. 400 characters max"),
-			], createComment);
+	[
+		check("comment[text]")
+		.trim().escape()
+		.not().isEmpty()
+		.withMessage("Comment is empty")
+		.isLength({
+			max: 800
+		}).withMessage("Too long. 800 characters max"),
+	], createComment);
 router.get("/:comment_id/edit", middleware.checkCommentOwnership, editComment);
 router.put("/:comment_id", middleware.checkCommentOwnership,
-			[
-			check("comment[text]")
-				.trim().escape()
-				.not().isEmpty()
-				.withMessage("Comment is empty")
-				.isLength({max:400}).withMessage("Comment is too long - 400 characters max"),
-			], updateComment);
+	[
+		check("comment[text]")
+		.trim().escape()
+		.not().isEmpty()
+		.withMessage("Comment is empty")
+		.isLength({
+			max: 400
+		}).withMessage("Comment is too long - 400 characters max"),
+	], updateComment);
 router.delete("/:comment_id", middleware.checkCommentOwnership, deleteComment);
 
 module.exports = router;
 
 // Comments New Route
-function newComment(req, res){
+function newComment(req, res) {
 	// find job by id
-	Job.findById(req.params.id, function(err, job){
-	   if(err){
-		   console.log(err);
-	   } else {
-		   res.render("comments/new", {job: job});
-	   }
+	Job.findById(req.params.id, function (err, job) {
+		if (err) {
+			console.log(err);
+		} else {
+			res.render("comments/new", {
+				job: job
+			});
+		}
 	});
 }
 
 // Comments Create Route
-function createComment(req, res){
-  	const errors = validationResult(req);
+function createComment(req, res) {
+	const errors = validationResult(req);
 	if (!errors.isEmpty()) {
-		errors.array().forEach(function(err){
+		errors.array().forEach(function (err) {
 			req.flash("error", err.msg);
 		});
 		res.redirect("/jobs/" + req.params.id + "/comments/new");
 	} else {
 		// lookup job using ID
-		Job.findById(req.params.id, function(err, job) {
-			if(err){
+		Job.findById(req.params.id, function (err, job) {
+			if (err) {
 				console.log(err);
 				res.redirect("/jobs");
 			} else {
 				// create new comment
-				Comment.create(req.body.comment, function(err, comment){
-					if(err){
-					   console.log(err);
+				Comment.create(req.body.comment, function (err, comment) {
+					if (err) {
+						console.log(err);
 					} else {
 						// add username and id to comment
-				        comment.author.id = req.user._id;
-				        comment.author.username = req.user.username;
-				        comment.author.fullName = req.user.fullName;
+						comment.author.id = req.user._id;
+						comment.author.username = req.user.username;
+						comment.author.fullName = req.user.fullName;
 						// connect new comment to job
 						comment.save();
 						job.modifiedAt = Date.now();
@@ -71,48 +82,52 @@ function createComment(req, res){
 						// redirect to job show page
 						req.flash("success", "Success! Comment added.");
 						res.redirect("/jobs/" + job._id);
-				   }
-			   });
-		   }
-	   });
+					}
+				});
+			}
+		});
 	}
 }
 
 // Comment Edit Route
-function editComment(req,res){
-	Comment.findById(req.params.comment_id, function(err, foundComment) {
-	    if(err){
-	    	console.log(err);
-	    } else {
-	    	Job.findById(req.params.id, function(err, foundJob) {
-	    	    if(err){
-	    	    	console.log(err);
-	    	    } else {
-	    	    	res.render("comments/edit", {job_id: req.params.id, job: foundJob, comment: foundComment});
-	    	    }
-	    	});
-	    }
+function editComment(req, res) {
+	Comment.findById(req.params.comment_id, function (err, foundComment) {
+		if (err) {
+			console.log(err);
+		} else {
+			Job.findById(req.params.id, function (err, foundJob) {
+				if (err) {
+					console.log(err);
+				} else {
+					res.render("comments/edit", {
+						job_id: req.params.id,
+						job: foundJob,
+						comment: foundComment
+					});
+				}
+			});
+		}
 	});
 }
 
 // Comment Update Route
-function updateComment(req, res){
+function updateComment(req, res) {
 	const errors = validationResult(req);
 	if (!errors.isEmpty()) {
-		errors.array().forEach(function(err){
+		errors.array().forEach(function (err) {
 			req.flash("error", err.msg);
 		});
 		res.redirect("/jobs/" + req.params.id + "/comments/" + req.params.comment_id + "/edit");
 	} else {
-		Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, function(err, updatedComment) {
-			if(err){
+		Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, function (err, updatedComment) {
+			if (err) {
 				res.redirect("back");
 			} else {
-				Job.findByIdAndUpdate(req.params.id, req.body.job, function(err, updatedJob){
-					if(err){
+				Job.findByIdAndUpdate(req.params.id, req.body.job, function (err, updatedJob) {
+					if (err) {
 						res.redirect("back");
 					} else {
-						res.redirect("/jobs/" + req.params.id );
+						res.redirect("/jobs/" + req.params.id);
 					}
 				});
 			}
@@ -121,10 +136,10 @@ function updateComment(req, res){
 }
 
 // Comment Destroy Route
-function deleteComment(req, res){
+function deleteComment(req, res) {
 	//findByIdAndRemove
-	Comment.findByIdAndRemove(req.params.comment_id, function(err){
-		if(err){
+	Comment.findByIdAndRemove(req.params.comment_id, function (err) {
+		if (err) {
 			res.redirect("back");
 		} else {
 			req.flash("success", "Success! Comment deleted.");
